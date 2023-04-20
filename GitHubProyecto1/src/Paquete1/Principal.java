cc834d926f60f1cec651bcc446e4369019f80786
Daw <Daw@DESKTOP-8OVFM6T>
2023-04-20 18:55:08 +0200

    Estos cambios son de creacion de proyecto y he añadido un syso.

---
+++ GitHubProyecto1/src/Paquete1/Principal.java
@@ -0,0 +1,12 @@
+package Paquete1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Estamos haciendo control de versiones con Git");
+		
+	}
+
+}
